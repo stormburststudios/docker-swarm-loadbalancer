b11945e593c32a8345114e6b38973f90c6da2bec
Matthew Baggett <matthew@baggett.me>
2019-07-23 13:15:19 +0200

    Move tail syslog to another thread

--- marshall/installers/101_rsyslog.sh
+++ marshall/installers/101_rsyslog.sh
@@ -2,6 +2,7 @@
 source /app/installers/config
 $APT_GET rsyslog
 
-mkdir /etc/service/rsyslog
+mkdir /etc/service/rsyslog /etc/service/rsyslog-show
 mv /app/etc/service/rsyslog/rsyslog.runit /etc/service/rsyslog/run
-chmod +x /etc/service/rsyslog/run
+mv /app/etc/service/rsyslog/show-rsyslog.runit /etc/service/rsyslog-show/run
+chmod +x /etc/service/rsyslog/run /etc/service/rsyslog-show/run

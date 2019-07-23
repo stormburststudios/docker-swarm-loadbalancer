2f44e066f00f55a808871cf15fbba4fd9df41be7
Matthew Baggett <matthew@baggett.me>
2019-07-23 14:23:43 +0200

    Fixed rsyslog piping.

--- marshall/installers/101_rsyslog.sh
+++ marshall/installers/101_rsyslog.sh
@@ -2,7 +2,6 @@
 source /app/installers/config
 $APT_GET rsyslog
 
-mkdir /etc/service/rsyslog /etc/service/rsyslog-show
+mkdir /etc/service/rsyslog
 mv /app/etc/service/rsyslog/rsyslog.runit /etc/service/rsyslog/run
-mv /app/etc/service/rsyslog/show-rsyslog.runit /etc/service/rsyslog-show/run
-chmod +x /etc/service/rsyslog/run /etc/service/rsyslog-show/run
+chmod +x /etc/service/rsyslog/run

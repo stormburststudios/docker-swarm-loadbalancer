0e4597d8c07d391d20a306ac3c3d15dedfd145d7
Matthew Baggett <matthew@baggett.me>
2021-04-12 11:12:07 +0200

    Mirrors + Upstream instead of just Mirrors.

--- marshall/installers/000_apt.sh
+++ marshall/installers/000_apt.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 source /installers/config
 echo "force-unsafe-io" > /etc/dpkg/dpkg.cfg.d/force-unsafe-io
-# swap http://archive.ubuntu.com for the new mirror://mirrors.ubuntu.com/mirrors.txt mirror-router.
-sed -i 's/http\:\/\/archive\.ubuntu\.com\/ubuntu\//mirror\:\/\/mirrors.ubuntu.com\/mirrors.txt/g' /etc/apt/sources.list
+
+# We're gonna move the sources to .d/ubuntu-upstream.list, then copy it, then manipulate it for a mirror list.
+mv /etc/apt/sources.list /etc/apt/sources.list.d/ubuntu-upstream.list
+cp /etc/apt/sources.list.d/ubuntu-upstream.list /etc/apt/sources.list.d/ubuntu-mirrors.list
+touch /etc/apt/sources.list
+sed -i 's/http\:\/\/archive\.ubuntu\.com\/ubuntu\//mirror\:\/\/mirrors.ubuntu.com\/mirrors.txt/g' /etc/apt/sources.list.d/ubuntu-mirrors.list
+sed -i "s|deb http://security.ubuntu.com|# deb http://security.ubuntu.com|g" /etc/apt/sources.list.d/ubuntu-mirrors.list
 
 # Update apt repos
 apt-get -qq update

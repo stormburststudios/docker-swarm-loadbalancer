49c06822ee39eecd02d8906f58a42836067ff5a0
Matthew Baggett <matthew@baggett.me>
2021-05-29 22:54:10 +0200

    Feature/rebuild for better caching (#3)
    * Rebuilding for better caching
    
    * Rebuilding for better caching. Squash this commit.
    
    * Paths to dockerfiles changed.
    
    * Rebuilding for better caching. Squash this commit.
    
    * Rebuilding for better caching. Squash this commit.
    
    * Refactoring
    
    * Refactoring
    
    * fixing the screaming about duplicates in apt sources.
    
    * Vanity tagging
    
    * Vanity tagging
    
    * Try vanity again.
    
    * re-enable all versions and variants

--- marshall/installers/000_apt.sh
+++ marshall/installers/000_apt.sh
@@ -2,12 +2,18 @@
 source /installers/config
 echo "force-unsafe-io" > /etc/dpkg/dpkg.cfg.d/force-unsafe-io
 
-# We're gonna move the sources to .d/ubuntu-upstream.list, then copy it, then manipulate it for a mirror list.
-mv /etc/apt/sources.list /etc/apt/sources.list.d/ubuntu-upstream.list
-cp /etc/apt/sources.list.d/ubuntu-upstream.list /etc/apt/sources.list.d/ubuntu-mirrors.list
-touch /etc/apt/sources.list
-sed -i 's/http\:\/\/archive\.ubuntu\.com\/ubuntu\//mirror\:\/\/mirrors.ubuntu.com\/mirrors.txt/g' /etc/apt/sources.list.d/ubuntu-mirrors.list
-sed -i "s|deb http://security.ubuntu.com|# deb http://security.ubuntu.com|g" /etc/apt/sources.list.d/ubuntu-mirrors.list
+# We're gonna move the sources to .d/ubuntu.list, then copy it, then manipulate it for a mirror list.
+(
+  cat /etc/apt/sources.list;
+  cat /etc/apt/sources.list \
+    | sed 's/http\:\/\/archive\.ubuntu\.com\/ubuntu\//mirror\:\/\/mirrors.ubuntu.com\/mirrors.txt/g' \
+    | sed "s|deb http://security.ubuntu.com|# deb http://security.ubuntu.com|g"
+) \
+  | sed '/^#/d' \
+  | sed '/^$/d' \
+  | uniq \
+  > /etc/apt/sources.list.d/ubuntu.list
+rm /etc/apt/sources.list; touch /etc/apt/sources.list;
 
 # Update apt repos
 apt-get -qq update

629fb64df300ebed228981ab2f9a2a0d92be3647
Matthew Baggett <matthew@baggett.me>
2024-01-05 18:16:21 +0100

    Significant improvements specifically to php_cs_fixer

--- bouncer/.php-cs-fixer.php
+++ bouncer/.php-cs-fixer.php
@@ -2,21 +2,39 @@
 $finder = PhpCsFixer\Finder::create();
 $finder->in(__DIR__);
 
-return (new PhpCsFixer\Config)
+return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setHideProgress(false)
     ->setRules([
-        '@PSR2' => true,
-        'strict_param' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        '@PhpCsFixer' => true,
-        '@PHP73Migration' => true,
-        'no_php4_constructor' => true,
-        'no_unused_imports' => true,
-        'no_useless_else' => true,
-        'no_superfluous_phpdoc_tags' => false,
-        'void_return' => true,
-        'yoda_style' => false,
+        '@PhpCsFixer'                      => true,
+        // '@PhpCsFixer:risky'                => true,
+        '@PHP82Migration'                  => true,
+        '@PHP80Migration:risky'            => true,
+        '@PSR12'                           => true,
+        '@PSR12:risky'                     => true,
+        '@PHPUnit100Migration:risky'       => true,
+
+        'binary_operator_spaces'     => [
+            'default'   => 'align_single_space_minimal',
+            'operators' => [
+                '='  => 'align_single_space',
+                '=>' => 'align_single_space',
+            ],
+        ],
+        'types_spaces'               => [
+            'space'                => 'single',
+            'space_multiple_catch' => 'single',
+        ],
+
+        // Annoyance-fixers:
+        'concat_space'               => ['spacing' => 'one'], // This one is a matter of taste.
+        'no_superfluous_phpdoc_tags' => [
+            'allow_mixed'         => false,
+            'allow_unused_params' => false,
+            'remove_inheritdoc'   => true,
+        ],
+        'yoda_style'                 => false, // Disabled as its annoying. Comes with @PhpCsFixer
+        'native_function_invocation' => false, // Disabled as adding count($i) -> \count($i) is annoying, but supposedly more performant
     ])
     ->setFinder($finder)
     ;
